0205fffb07e7a1a8abc569777590826be7d4a6c7
samra-e <samrawitesehetu@gmail.com>
2026-01-12 09:56:35 +0300

    Solved Same Tree Problem

--- binaryTreeInorderTraversal.tsx
+++ binaryTreeInorderTraversal.tsx
@@ -1,13 +1,4 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+
 
 function inorderTraversal(root: TreeNode | null): number[] {
   const result: number[] = [];
